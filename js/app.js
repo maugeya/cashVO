2ffb186c90547f62b7b67c153c928bd4c86968ff
Angela Maugey <a.maugey@gmail.com>
2018-08-05 17:30:38 +0100

    Change carousel speed

--- js/app.js
+++ js/app.js
@@ -1,29 +1,30 @@
 $(() => {
   console.log('js loaded');
 
-
-
   $('.nav-link').on('click', scrollToSection);
   $('.navbar-brand').on('click', scrollToSection);
   $('.icon').on('click', scrollToSection);
 
-  function scrollToSection () {
-
+  function scrollToSection() {
     const section = $(this).attr('href');
-    $('body', 'html').animate({
-      scrollTop: $(section).offset().top - 40
-    }, 1300);
+    $('body', 'html').animate(
+      {
+        scrollTop: $(section).offset().top - 40
+      },
+      1300
+    );
   }
 
-
-  $(function () {
-    $(window).scroll(updateHeader).trigger('scroll');
+  $(function() {
+    $(window)
+      .scroll(updateHeader)
+      .trigger('scroll');
 
     function updateHeader() {
       const viewportHeight = $(window).height();
       const scrollTop = $(window).scrollTop();
 
-      if(scrollTop >= viewportHeight) {
+      if (scrollTop >= viewportHeight) {
         $('nav').addClass('translucent');
         // $('nav').removeClass('topbar');
       } else {
@@ -31,24 +32,22 @@ $(() => {
         // $('nav').addClass('topbar');
       }
     }
-
   });
 
   (function() {
-
     var quotes = $('.quotes');
     var quoteIndex = -1;
 
     function showNextQuote() {
       ++quoteIndex;
-      quotes.eq(quoteIndex % quotes.length)
-      .fadeIn(2000)
-      .delay(2000)
-      .fadeOut(2000, showNextQuote);
+      quotes
+        .eq(quoteIndex % quotes.length)
+        .fadeIn(2000)
+        .delay(2000)
+        .fadeOut(2000, showNextQuote);
     }
 
     showNextQuote();
-
   })();
 
   $(function() {
@@ -58,42 +57,22 @@ $(() => {
     function setImgHeight() {
       imgHeight = $('.carousel .carousel-item.active img').height();
       $('.carousel-img').height(imgHeight);
-      console.log(imgHeight);
     }
 
     // Initialize the height
     // setTimeout to wait until the image is loaded
-    setTimeout( function(){
+    setTimeout(function() {
       setImgHeight();
-    }, 1000 );
+    }, 1000);
 
     // Recalculate the height if the screen is resized
-    $( window ).resize(function() {
+    $(window).resize(function() {
       setImgHeight();
     });
   });
 
   $('.carousel').carousel();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  setInterval(function() {
+    $('.carousel').carousel('next');
+  }, 800);
 });
