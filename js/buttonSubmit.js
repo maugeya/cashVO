360fb8bb3264b5b3f81cd6ed1d100251222dfa9f
Angela Maugey <a.maugey@gmail.com>
2018-10-13 18:00:36 +0100

    Changing form

---
+++ js/buttonSubmit.js
@@ -0,0 +1,7 @@
+$(() => {
+  console.log('carousel loading');
+  $('#contact-form').submit(function(e) {
+    console.log(e);
+    console.log('submitted');
+  });
+});
--- js/carousel.js
+++
@@ -1,3 +0,0 @@
-$(() => {
-  console.log('carousel loading');
-});
