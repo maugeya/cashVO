a625061c4f22c726fb8d6d377f4459eb6cc906fa
Angela Maugey <a.maugey@gmail.com>
2018-10-13 16:17:26 +0100

    removing carousel :(

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,3 @@
 $(() => {
   console.log('carousel loading');
-  $('.your-class').slick({
-    dots: true,
-    autoplay: true,
-    infite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
 });
